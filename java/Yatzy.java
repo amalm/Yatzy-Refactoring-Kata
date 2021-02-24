9a46bc740a32caf9ef06cbe4c5aba69075fb9ce7
Anders Malmborg <anders.malmborg@skidata.com>
2021-02-24 19:30:35 +0100

    Fix yatzy

--- java/Yatzy.java
+++ java/Yatzy.java
@@ -15,13 +15,13 @@ public class Yatzy
 		return total;
 	}
 
-	public static int yatzy(int... dice)
+	public static int yatzy(int d1, int d2, int d3, int d4, int d5)
 	{
-		List<Integer> dices = Arrays.asList(dice);
+		List<Integer> dices = Arrays.asList(d1, d2, d3, d4, d5);
 		int number = dices.get(0);
-		for (int dice : dices)
+		for (int d : dices)
 		{
-			if (dice != number)
+			if (d != number)
 			{
 				return 0;
 			}
